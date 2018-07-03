30efb9b3416d94b63e00578566a57bbdff1c57b6
Mambu38 <39092278+Mambu38@users.noreply.github.com>
2018-07-03 11:30:11 +0200

    Added a config file

---
+++ config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module is used as a config file to enable/disable some functionnalities.
+
+Copyright (C) 2018  Thomas Vigouroux
+
+This file is part of CALOA.
+
+CALOA is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CALOA is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CALOA.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+AUTO_BUG_REPORT_ENABLED = True
+
+AUTO_UPDATE_ENABLED = True
+
+DEVELOPER_MODE_ENABLED = True
