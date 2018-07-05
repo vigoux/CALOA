792989fd72adc3366e7b3a03f8c9ff7a98d52c24
Mambu38 <39092278+Mambu38@users.noreply.github.com>
2018-07-05 09:40:40 +0200

    Documented config file.

--- config.py
+++ config.py
@@ -21,8 +21,28 @@ You should have received a copy of the GNU General Public License
 along with CALOA.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+"""
+By modifying this file you can custom CALOA fundamental comportment.
+Warning : each update will reset this file, and you will have to modified it
+again.
+
+All constants defined here shall be set either to True or to False.
+"""
+
+# if AUTO_BUG_REPORT_ENABLED is set to True CALOA will send a bug report
+# containing information about your platform, your python distribution and
+# precise informations about the bug. Logs will not be sent for privacy reasons
+# but for developers to have better informations about how bug happens
+# you can send your log file to caloareportsender@gmail.com.
 AUTO_BUG_REPORT_ENABLED = True
 
+# if AUTO_UPDATE_ENABLED is set to True, CALOA will download the latest STABLE
+# version. Update take some seconds but you will need an internet connection.
 AUTO_UPDATE_ENABLED = True
 
+# if DEVELOPER_MODE_ENABLED is set to True, CALOA will download the latest
+# version and will enable AUTO_BUG_REPORT. Be carefull, by enabling this
+# feature CALOA may be unstable, but by enabling this feature, each bug
+# you collide with will give precious informations about the bug and will
+# increase the patch rate.
 DEVELOPER_MODE_ENABLED = True
